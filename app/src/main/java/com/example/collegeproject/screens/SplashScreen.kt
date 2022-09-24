ca6e9aabaf0b4cdeb1430388006905a47de25b43
Home <praveen.sahu7512@gmail.com>
2022-09-24 15:21:22 -0400

    code cleanup

--- app/src/main/java/com/example/collegeproject/screens/SplashScreen.kt
+++ app/src/main/java/com/example/collegeproject/screens/SplashScreen.kt
@@ -1,23 +1,9 @@
 package com.example.collegeproject.screens
-
-
 import com.example.collegeproject.ui.theme.CollegeProjectTheme
-
 import android.content.Intent
 import android.os.Bundle
-
-import android.view.Window
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-
-import androidx.compose.foundation.layout.padding
-import androidx.compose.runtime.Composable
-
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.*
 import com.example.collegeproject.utils.UtilsFunctions
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
